eddf662f10d5a6ec66bfde7ff238e880a695b487
Himanshu kumar <himanshu231204@gmail.com>
2026-01-23 00:07:02 +0530

    Prepare AutoML Studio Pro for Streamlit Cloud deployment

--- app.py
+++ app.py
@@ -401,3 +401,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # ---------------- FOOTER ----------------
+st.divider()
+
+st.markdown(
+    """
+    <div style="text-align:center; font-size:14px;">
+        👨‍💻 Developed by <b>Himanshu Kumar</b><br><br>
+        🔗 
+        <a href="https://www.linkedin.com/in/himanshu231204" target="_blank">LinkedIn</a> |
+        <a href="https://github.com/himanshu231204" target="_blank">GitHub</a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
